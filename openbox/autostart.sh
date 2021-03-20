1a235ec5b8702a317e793bf5df19fed346c92403
misketmuskat <71901736+misketmuskat@users.noreply.github.com>
2021-03-20 10:23:47 +0300

    Add files via upload

---
+++ openbox/autostart.sh
@@ -0,0 +1,20 @@
+nitrogen --restore &
+
+tint2 &
+
+compton &
+
+tlp start &
+
+sleep 2 && dunst &
+
+sleep 3 && dunstify &
+
+conky -q -c ~/.config/conky/conky.conf &
+
+sleep 2 && ~/.applications/greenclip daemon &
+
+xset +fp /usr/local/share/fonts &
+
+xset fp rehash &
+
